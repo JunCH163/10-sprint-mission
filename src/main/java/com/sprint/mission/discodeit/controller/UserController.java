3774777f705beea4ebcd24567dd96bd135871ee3
JunCH163 <ckdgus163@gmail.com>
2026-02-13 20:09:01 +0900

    refactor: sprint5 기초 세팅 및  rebase하면서 충돌났던거 해결

--- src/main/java/com/sprint/mission/discodeit/controller/UserController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/UserController.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
     private final UserStatusService userStatusService;
