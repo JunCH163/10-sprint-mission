cfaf97c6146c6a4497e73efbefa8464d1886de07
JunCH163 <ckdgus163@gmail.com>
2026-05-21 09:44:01 +0900

    refactor: 사용자 DTO의 온라인 상태 매핑 방식을 분리

--- src/main/java/com/sprint/mission/discodeit/dto/data/UserDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/data/UserDto.java
@@ -1,6 +1,7 @@
 package com.sprint.mission.discodeit.dto.data;
 
 import com.sprint.mission.discodeit.entity.Role;
+import com.sprint.mission.discodeit.entity.User;
 
 import java.util.UUID;
 
@@ -12,5 +13,18 @@ public record UserDto(
     BinaryContentDto profile,
     Boolean online
 ) {
-
+    public static UserDto of(
+            User user,
+            BinaryContentDto profile,
+            boolean online
+    ) {
+        return new UserDto(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getRole(),
+                profile,
+                online
+                );
+    }
 }
